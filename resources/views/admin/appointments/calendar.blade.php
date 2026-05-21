f814e4069cd63d6d5448580938d83eeae0c880fb
Kh.Ezzat <macair@MacBook-Air.local>
2026-05-21 18:56:21 +0300

    Add production appointment admin views

---
+++ resources/views/admin/appointments/calendar.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1>Appointments Calendar</h1>
+</div>
+@endsection
---
+++ resources/views/admin/appointments/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1>Create Appointment</h1>
+</div>
+@endsection
